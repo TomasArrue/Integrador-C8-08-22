29e9fc759727c1f6078500b11468246ccff0f162
TomasArrue <tomasaugustoarrue@gmail.com>
2022-09-15 18:33:17 -0300

    primer comit

--- scripts/signup.js
+++ scripts/signup.js
@@ -1,31 +1,76 @@
-window.addEventListener('load', function () {
-    /* ---------------------- obtenemos variables globales ---------------------- */
-   
+window.addEventListener("load", function () {
+  /* ---------------------- obtenemos variables globales ---------------------- */
+  const form = document.querySelector("form");
+  const inputNombre = document.getElementById("inputNombre");
+  const inputApellido = document.getElementById("inputApellido");
+  const inputEmail = document.getElementById("inputEmail");
+  const inputPassword = document.getElementById("inputPassword");
+  const inputPasswordRepetida = document.getElementById(
+    "inputPasswordRepetida"
+  );
 
+  let mensajeArray = [];
 
-    
+  /* -------------------------------------------------------------------------- */
+  /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
+  /* -------------------------------------------------------------------------- */
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-    /* -------------------------------------------------------------------------- */
-    /*            FUNCIÓN 1: Escuchamos el submit y preparamos el envío           */
-    /* -------------------------------------------------------------------------- */
-    form.addEventListener('submit', function (event) {
-      
-
-
-
-
-    });
-
-    /* -------------------------------------------------------------------------- */
-    /*                    FUNCIÓN 2: Realizar el signup [POST]                    */
-    /* -------------------------------------------------------------------------- */
-    function realizarRegister(settings) {
-        
-
-
-
-
+    const datosRegistro = {
+      firstName: normalizarTexto(inputNombre.value),
+      lastName: normalizarTexto(inputApellido.value),
+      email: normalizarEmail(inputEmail.value),
+      password: inputPassword.value,
+      passwordConfirm: inputPasswordRepetida.value,
     };
 
+    const mensajeFallido = validarEmail(datosRegistro.email);
+    const contraseniaIguales = compararContrasenias(
+      datosRegistro.password,
+      datosRegistro.passwordConfirm
+    );
+    const logitudContrasenia = validarContrasenia(datosRegistro.password);
+    mensajeArray.push(mensajeFallido, contraseniaIguales, logitudContrasenia);
 
+    if (permitirRegistro(mensajeArray)) {
+      realizarRegister(datosRegistro);
+    } else {
+      const containerError = document.querySelector(".containerError");
+      containerError.innerHTML = ``;
+      mensajeArray.forEach((element) => {
+        if (element !== undefined) {
+          containerError.innerHTML += `<li class="rafa2">${element}</li>`;
+        }
+      });
+    }
+
+    mensajeArray = [];
+  });
+
+  /* -------------------------------------------------------------------------- */
+  /*                    FUNCIÓN 2: Realizar el signup [POST]                    */
+  /* -------------------------------------------------------------------------- */
+
+  function realizarRegister(datosRegistro) {
+    const URL = "https://ctd-todo-api.herokuapp.com/v1/users";
+    const config = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify(datosRegistro),
+    };
+
+    fetch(URL, config)
+      .then(function (res) {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch(function (e) {
+        alert("Error! intente mas tarde");
+      });
+  }
 });
